d5283d039ffb80bb754e9c603e3df3079b13073d
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-09-19 12:54:38 -0400

    Section 5: working with components, props, jsx COMPLETED

--- src/index.js
+++ src/index.js
@@ -79,12 +79,19 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
+     
+
 {numPizzas > 0 && (
+  <>
+  <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stove oven, all organic, all delicious.</p>
+
   <ul className="pizzas">
       {pizzas.map(pizza => (
         <Pizza pizzaObj={pizza} key={pizza.name} />
         ))}
       </ul>
+  </>
+  
 )}
 
 {/* Ternary operator 
@@ -117,17 +124,19 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
 // Condinitonal rendering with multiple returns
-  if (props.pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
+
+
 
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-      <h3>{props.pizzaObj.name}</h3>
-      <p>{props.pizzaObj.ingredients}</p>
-      <span>{props.pizzaObj.price}</span>
+      <h3>{pizzaObj.name}</h3>
+      <p>{pizzaObj.ingredients}</p>
+      <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -145,13 +154,19 @@ function Footer() {
     <footer className="footer">
     
       {isOpen ? (
-        <div className="order">
-        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-      <button className="btn">Order</button>
-      </div> 
+        <Order closeHour={closeHour} openHour={openHour}/>
       ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
     </footer>
   );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+        <p>We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+      </div> 
+  )
 }
 
 // React v18
